0e996dfb5e9e19e826db3075fb8447994d04db5d
Tiffany Lam <tlam5@sbcglobal.net>
2019-05-30 13:53:48 -0700

    Change 'App' to 'Game'

---
+++ src/Game.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Game.css';
+
+function Game() {
+  return (
+    <div className="Game">
+      <h1>Tic Tac Toe</h1>
+    </div>
+  );
+}
+
+export default Game;
