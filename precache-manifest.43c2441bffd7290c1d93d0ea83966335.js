29599b2d8bd7f5ac209e3c29bf58f7a94acde310
Tiffany Lam <tlam5@sbcglobal.net>
2019-10-08 14:24:35 -0700

    Updates

---
+++ precache-manifest.43c2441bffd7290c1d93d0ea83966335.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "744ae0fc2f4354cf045512a6d16ff742",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "1a3dc989a93dc8e036dd",
+    "url": "/tictactoe/static/css/main.8b327db8.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/tictactoe/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "1a3dc989a93dc8e036dd",
+    "url": "/tictactoe/static/js/main.0ba11234.chunk.js"
+  },
+  {
+    "revision": "e9d42febdd2440e2e0cd",
+    "url": "/tictactoe/static/js/runtime~main.1cfd47c4.js"
+  }
+]);
